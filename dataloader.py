a220bd0eb944d58475e35eb009f28198bf509002
rmool <armin2saadat@gmail.com>
2021-10-24 16:39:28 +0330

    complete dataloader.

--- dataloader.py
+++ dataloader.py
@@ -1,15 +1,26 @@
+import os
+import pickle
+
 from torch.utils.data import Dataset
 from torch.utils.data import DataLoader
+
+from path_definition import DATA_DIR
 
 
 class Dataset_(Dataset):
     def __init__(self):
 
-        with open('./data/images', 'rb') as f:
+        # read from file
+        with open(os.path.join(DATA_DIR, 'images'), 'rb') as f:
             images = pickle.load(f)
-
-        with open('./data/labels', 'rb') as f:
+        with open(os.path.join(DATA_DIR, 'labels'), 'rb') as f:
             labels = pickle.load(f)
+
+        # normalize
+        for i, img in enumerate(images):
+            images[i] = (img / img.max()).float()
+        for i, lb in enumerate(labels):
+            labels[i] = (lb / lb.max()).float()
 
         self.images = images
         self.labels = labels
@@ -24,8 +35,8 @@ class Dataset_(Dataset):
         return tuple(outputs)
 
 
-def get_dataloader(images, labels, batch_size, shuffle=False, pin_memory=False, workers=0):
-    dataset = Dataset_(images, labels)
+def get_dataloader(batch_size, shuffle=False, pin_memory=False, workers=0):
+    dataset = Dataset_()
     dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=shuffle, pin_memory=pin_memory, num_workers=workers)
 
     return dataloader
