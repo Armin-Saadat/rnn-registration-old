e32fb076ad5da0c47bf02ec679a3ff5d67ea0607
rmool <armin2saadat@gmail.com>
2021-10-25 01:08:24 +0330

    get id.

--- api/evaluate.py
+++ api/evaluate.py
@@ -10,19 +10,20 @@ from factory.evaluator import Evaluator
 
 def run(args):
     dataloader = get_dataloader(args.batch_size, args.shuffle, args.pin_memory, args.num_workers)
-    snapshot_path = os.path.join(OUTPUT_DIR, args.snapshot)
+    snapshot_path = os.path.join(OUTPUT_DIR, args.id, args.snapshot)
     snapshot = torch.load(snapshot_path, map_location='cpu')
     model = Bottleneck(dataloader.dataset.image_size)
     model.load_state_dict(snapshot['model_state_dict'])
     model = model.to(args.device)
 
-    evaluator = Evaluator(1, model, dataloader)
+    evaluator = Evaluator(args.id, model, dataloader)
     evaluator.evaluate()
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='args')
-    parser.add_argument('-snapshot', type=str, help='path to the saved snapshot.')
+    parser.add_argument('-id', type=str, help='id of your experiment.')
+    parser.add_argument('-snapshot', type=str, help='name of the saved snapshot.')
     parser.add_argument('-device', type=str, default='cuda', help='cpu or cuda')
     parser.add_argument('-batch_size', type=int, default=1, help='batch_size')
     parser.add_argument('-shuffle', type=bool, default=False, help='shuffle')
@@ -30,7 +31,10 @@ if __name__ == '__main__':
     parser.add_argument('-num_workers', type=int, default=0, help='num_workers')
     args = parser.parse_args()
 
+    if args.id is None:
+        raise Exception('Please specify an ID for your run.')
+
     if args.snapshot is None:
-        raise Exception('Please specify the path to your snapshot.')
+        raise Exception('Please specify the name of your snapshot.')
 
     run(args)
