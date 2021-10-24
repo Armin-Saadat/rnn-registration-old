66df219a18c4595942735bed7b3c22529051fda3
rmool <armin2saadat@gmail.com>
2021-10-24 17:34:53 +0330

    set ne backend.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from utils.dataloader import get_dataloader
 from utils.visualizer import visualize
 
 if __name__ == '__main__':
+
     batch_size = 1
     shuffle = False
     pin_memory = False
@@ -10,5 +11,3 @@ if __name__ == '__main__':
     dataloader = get_dataloader(batch_size, shuffle, pin_memory, num_workers)
     for images, labels in dataloader:
         visualize(images, labels)
-
-
--- utils/visualizer.py
+++ utils/visualizer.py
@@ -2,6 +2,7 @@ import neurite as ne
 
 
 def visualize(image, label):
+    print(image.shape)
     pics = [image, label]
     titles = ['image', 'label']
     ne.plot.slices(pics, titles=titles, cmaps=['gray'], do_colorbars=True, imshow_args=[{'origin': 'lower'}]);
