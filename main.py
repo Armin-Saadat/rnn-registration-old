a7cf8e03d90145a649a32ae486f4c4f6b0e90f41
rmool <armin2saadat@gmail.com>
2021-10-24 16:50:35 +0330

    create small dataset to check quickly.

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+import os
+import pickle
+
+from path_definition import DATA_DIR
 from dataloader import get_dataloader
 
 if __name__ == '__main__':
@@ -9,5 +13,11 @@ if __name__ == '__main__':
     dataloader = get_dataloader(batch_size, shuffle, pin_memory, num_workers)
     for images, labels in dataloader:
         print(images.shape)
+        with open(os.path.join(DATA_DIR, 'images_check'), 'wb') as f:
+            pickle.dump(images, f)
+
+        print(labels.shape)
+        with open(os.path.join(DATA_DIR, 'labels_check'), 'wb') as f:
+            pickle.dump(labels, f)
 
         exit()
