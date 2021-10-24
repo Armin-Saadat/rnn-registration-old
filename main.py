642356eda15901584c299f976f8e85a5b2850d92
rmool <armin2saadat@gmail.com>
2021-10-24 23:49:30 +0330

    add arguments to parser.

--- main.py
+++ main.py
@@ -11,11 +11,15 @@ def run(args):
     for images, labels in dataloader:
         images = images.permute(1, 0, 2, 3).unsqueeze(2).to(args.device)
         labels = labels.permute(1, 0, 2, 3).unsqueeze(2).to(args.device)
-        model(images)
+        model(images, labels)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='args')
+    parser.add_argument('-lr', type=float, default=0.001, help='learning-rate')
+    parser.add_argument('-epochs', type=int, default=100, help='number of epochs')
+    parser.add_argument('-smooth_weight', type=float, default=0.01, help='smooth weight')
+    parser.add_argument('-loss_type', type=str, default='mse', help='type of loss function')
     parser.add_argument('-device', type=str, default='cuda', help='cpu or cuda')
     parser.add_argument('-batch_size', type=int, default=1, help='batch_size')
     parser.add_argument('-shuffle', type=bool, default=False, help='shuffle')
