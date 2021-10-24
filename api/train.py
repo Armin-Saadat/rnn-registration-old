348d226436e4e071cf05ff2d90de1e9687fbdbfb
rmool <armin2saadat@gmail.com>
2021-10-25 00:52:08 +0330

    debug.

--- api/train.py
+++ api/train.py
@@ -11,7 +11,7 @@ def run(args):
     dataloader = get_dataloader(args.batch_size, args.shuffle, args.pin_memory, args.num_workers)
     model = Bottleneck(dataloader.dataset.image_size).to(args.device)
     optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
-    sim_loss = NCC().loss if args.loss == 'ncc' else MSE().loss
+    sim_loss = NCC().loss if args.sim_loss == 'ncc' else MSE().loss
     smooth_loss = Grad('l2').loss
     seg_loss = MSE().loss
 
@@ -24,10 +24,10 @@ if __name__ == '__main__':
     parser.add_argument('-id', type=str, help='name or number of the experiment')
     parser.add_argument('-lr', type=float, default=0.001, help='learning-rate')
     parser.add_argument('-epochs', type=int, default=100, help='number of epochs')
+    parser.add_argument('-sim_loss', type=str, default='mse', help='type of similarity-loss function')
     parser.add_argument('-sim_w', type=float, default=1, help='weight of similarity-loss')
     parser.add_argument('-smooth_w', type=float, default=0.01, help='weight of smooth-loss')
     parser.add_argument('-seg_w', type=float, default=0, help='weight of segmentation-loss')
-    parser.add_argument('-loss_type', type=str, default='mse', help='type of loss function')
     parser.add_argument('-device', type=str, default='cuda', help='cpu or cuda')
     parser.add_argument('-batch_size', type=int, default=1, help='batch_size')
     parser.add_argument('-shuffle', type=bool, default=False, help='shuffle')
