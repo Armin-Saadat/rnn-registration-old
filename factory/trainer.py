6e61e1e2337537b93b3f678c8d313760127747af
rmool <armin2saadat@gmail.com>
2021-10-25 00:15:23 +0330

    refactor.

--- factory/trainer.py
+++ factory/trainer.py
@@ -23,40 +23,41 @@ class Trainer:
             epoch_sim_loss = 0
             epoch_smooth_loss = 0
             epoch_seg_loss = 0
-            total_data_count = 0
+            epoch_data_n = 0
             epoch_start_time = time.time()
 
-            for images, labels in self.dataloader:
-                bs = images.shape[0]
+            for imgs, lbs in self.dataloader:
+                bs = imgs.shape[0]
                 # shape of imgs/lbs: (bs, seq_size, W, H) --> (seq_size, bs, 1, W, H)
-                images = images.permute(1, 0, 2, 3).unsqueeze(2).to(self.args.device)
-                labels = labels.permute(1, 0, 2, 3).unsqueeze(2).to(self.args.device)
+                imgs = imgs.permute(1, 0, 2, 3).unsqueeze(2).to(self.args.device)
+                lbs = lbs.permute(1, 0, 2, 3).unsqueeze(2).to(self.args.device)
 
                 # shape of moved_imgs/moved_lbs: (seq_size - 1, bs, 1, W, H)
                 # shape of flow: (seq_size - 1, bs, 2, W, H)
-                moved_imgs, moved_lbs, flow = self.model(images, labels)
+                moved_imgs, moved_lbs, flow = self.model(imgs, lbs)
 
                 # calculate loss
                 loss = 0
                 for i in range(bs):
-                    sim_loss = sim_loss_func(imgs[1:, i], moved_imgs[:, i])
-                    smooth_loss = smooth_loss_func(zero_phi, flows[:, i])
-                    seg_loss = seg_loss_func(lbs[1:, i], moved_lbs[:, i])
-                    loss = sim_loss + (smooth_weight * smooth_loss) + (seg_weight * seg_loss)
+                    sim_loss = self.sim_loss(imgs[1:, i], moved_imgs[:, i])
+                    smooth_loss = self.smooth_loss(zero_phi, flow[:, i])
+                    seg_loss = self.seg_loss(lbs[1:, i], moved_lbs[:, i])
+                    loss = (self.args.sim_w * sim_loss) + (self.args.smooth_w * smooth_loss) + (
+                                self.args.seg_w * seg_loss)
 
-                    # backpropagate and optimize
-                    optimizer.zero_grad()
-                    loss.backward()
-                    optimizer.step()
+                # backpropagate and optimize
+                self.optimizer.zero_grad()
+                loss.backward()
+                self.optimizer.step()
 
-                    # update epoch loss
-                    epoch_loss += loss * bs
-                    epoch_loss_sim += sim_loss * bs
-                    epoch_loss_smooth += smooth_loss * bs
-                    epoch_loss_seg += seg_loss * bs
-                    total_data_count += bs
+                # update epoch loss
+                epoch_loss += loss * bs
+                epoch_sim_loss += sim_loss * bs
+                epoch_smooth_loss += smooth_loss * bs
+                epoch_seg_loss += seg_loss * bs
+                epoch_data_n += bs
 
-                    # print epoch info
+                # print epoch info
                 loss_history.append(epoch_loss / total_data_count)
                 msg = 'epoch %d/%d, ' % (epoch + 1, args.epochs)
                 msg += 'loss= %.4e, ' % (epoch_loss / total_data_count)
@@ -65,3 +66,7 @@ class Trainer:
                 msg += 'seg_loss= %.4e, ' % (epoch_loss_seg / total_data_count)
                 msg += 'time= %.4f, ' % (time.time() - epoch_start_time)
                 print(msg, flush=True)
+
+    def __train(self):
+
+
